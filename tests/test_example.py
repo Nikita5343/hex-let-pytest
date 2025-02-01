06ed224b2772436cb6485329497a79f6153b61b6
Никита Войтов <voitov@MacBook-Air-Nikita.local>
2025-02-01 18:12:13 +0300

    add tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,9 +1,35 @@
 from hexlet_pytest.example import reverse
+import pytest
 
 
-def test_reverse():
-    assert reverse('Hexlet') == 'telxeH'
+def test_stack():
+    stack = []
 
+    stack.append("one")
+    stack.append("two")
 
-def test_reverse_for_empty_string():
-    assert reverse('') == ''
+    assert stack.pop() == 'two'
+
+def test_stack2():
+    stack = []
+
+    stack.append("one")
+    stack.append("two")
+
+    stack.pop()
+    assert stack.pop() == 'one'
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert (stack)
+
+    stack.pop()
+    assert not stack
+
+def test_popisempty():
+    stack = []
+
+    with pytest.raises(IndexError):
+        stack.pop()
